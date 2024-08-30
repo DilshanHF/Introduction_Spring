41fe6c38bf421ee86705a141655d9db4352e3438
DILSHAN <dilshanfonseka76@gmail.com>
2024-08-30 22:45:40 +0530

    done

---
+++ src/main/java/lk/ijse/customAnnotation/Nail.java
@@ -0,0 +1,13 @@
+package lk.ijse.customAnnotation;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Nail {
+}
